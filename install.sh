89bbc83ae38255c63228e50193f8585820dd5ee0
Tobias <tobiaspalmer93@gmail.com>
2018-03-31 15:38:12 +0200

    install ebn

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ echo "== Cloning installation"
 git -C /opt/easy-banano-node pull || git clone https://github.com/BitDesert/easy-banano-node.git /opt/easy-banano-node
 
 echo "== Starting installation"
-sudo bash /opt/easy-banano-node/enn/setup.sh
+sudo bash /opt/easy-banano-node/ebn/setup.sh
